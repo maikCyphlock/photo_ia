31f33748ac51c5ab444bd6fe22b85cdf1abe751d
maikCyphlock <maikolaguilar656@gmail.com>
2023-02-27 20:47:23 -0400

    to fix

--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -1,19 +1,17 @@
 'use client'
 import React from 'react'
-import { Uploader } from 'uploader'
-import { UploadDropzone } from 'react-uploader'
+import Uploady from '@rpldy/uploady'
+import UploadPreview from '@rpldy/upload-preview'
+import UploadButton from '@rpldy/upload-button'
 
-const uploader = Uploader({
-  apiKey: 'free' // Get production API keys from Upload.io
-})
-const options = { multi: true }
 function DropZone () {
   return (
-    <UploadDropzone uploader={uploader}
-    options={options}
-    onUpdate={files => files.map(x => console.log(x.originalFile))}
-    width="600px"
-    height="375px" />
+        <Uploady destination={{ url: 'https://my-server.com/upload' }}>
+            <UploadButton />
+
+            
+   
+        </Uploady>
   )
 }
 
