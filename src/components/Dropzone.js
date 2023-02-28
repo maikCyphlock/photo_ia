3ebce9d5a54ada4751ea37c881715d6df2dd5281
maikCyphlock <maikolaguilar656@gmail.com>
2023-02-27 21:27:21 -0400

    asd

--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -1,17 +1,34 @@
 'use client'
 import React from 'react'
-import Uploady from '@rpldy/uploady'
+import Uploady, { useItemProgressListener } from '@rpldy/uploady'
 import UploadPreview from '@rpldy/upload-preview'
 import UploadButton from '@rpldy/upload-button'
+import UploadDropZone from "@rpldy/upload-drop-zone";
+
+const cloudinary = new Cloudinary({
+    cloud: {
+      cloudName: "midudev",
+    },
+    url: {
+      secure: true,
+    },
+  })
+//must be rendered inside <Uploady>
+const LogProgress = () => {
+    useItemProgressListener((item) => {
+        console.log(`>>>>> (hook) File ${item.file.name} completed: ${item.completed}`);
+    });
+
+    return null;
+}
 
 function DropZone () {
   return (
-        <Uploady destination={{ url: 'https://my-server.com/upload' }}>
-            <UploadButton />
-
-            
-   
-        </Uploady>
+    <Uploady
+    destination={{ url: "https://my-server/upload" }}>
+    <LogProgress/>   
+    <UploadButton/>
+</Uploady>
   )
 }
 
