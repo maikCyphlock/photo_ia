e9aaac7a460d11bc427f97949134b0381329832a
maikCyphlock <maikolaguilar656@gmail.com>
2023-02-27 20:17:23 -0400

    adding landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,30 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
+import CloudinaryPatreon from '@/components/CloudinaryPatreon'
+import Dropzone from '@/components/Dropzone'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home () {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+        <>
+            <div style={inter.style} className="min-h-screen">
+                <main className="relative container grid grid-cols-2 place-content-center min-h-screen mx-auto max-w-lg min-w-screen">
+                    <h1 className="z-10 font-bold text-5xl md:text-6xl text-slate-700">
+                        Edita tus Imagenes con{' '}
+                        <span className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-t from-yellow-400 to-rose-600">
+                            IA
+                        </span>
+                    </h1>
+                    <img
+                        className="z-0"
+                        src="https://sb.kaleidousercontent.com/67418/658x756/361846cee7/all-pages-2.png "
+                        alt=""
+                    />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                    <CloudinaryPatreon />
+                </main>
+                <Dropzone/>
+            </div>
+        </>
   )
 }
