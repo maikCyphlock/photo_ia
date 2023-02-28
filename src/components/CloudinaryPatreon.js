ac1595bae41b67905893e8f323632cf5077c3eca
maikCyphlock <maikolaguilar656@gmail.com>
2023-02-27 20:17:38 -0400

    cloudinary partner

---
+++ src/components/CloudinaryPatreon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import CloudinaryLogo from '@/components/CloudinaryLogo'
+function CloudinaryPatreon () {
+  return (
+    <footer class="flex justify-center col-span-2 items-center gap-x-2 font-semibold pt-10">
+    Patrocinado por
+    <a
+        href="https://cloudinary.com"
+        target="_blank"
+        rel="noreferrer"
+    >
+        <CloudinaryLogo />
+    </a>
+</footer>
+  )
+}
+
+export default CloudinaryPatreon
