f9f142243cccfe03aeb648a6946389b8f1c7da93
maikCyphlock <maikolaguilar656@gmail.com>
2023-03-03 10:13:20 -0400

    feat: :sparkles: add dropzone component

--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -1,11 +1,13 @@
 'use client'
+import { API_KEY, CLOUD_NAME, UPLOAD_PRESET } from '@/consts/settings'
 import { Cloudinary } from '@cloudinary/url-gen'
 import Dropzone from 'dropzone'
 import 'dropzone/dist/dropzone.css'
 import { useEffect, useRef } from 'react'
+
 const cloudinary = new Cloudinary({
   cloud: {
-    cloudName: 'dsgixewc9'
+    cloudName: CLOUD_NAME
   },
   url: {
     secure: true
@@ -20,44 +22,42 @@ export default function DropZone () {
       inputRef.current = new Dropzone('#dropzone', {
         uploadMultiple: false,
         acceptedFiles: '.jpg,.png,.webp',
-        maxFiles: 1
+        maxFiles: 1,
+        addRemoveLinks: true
       })
     }
     console.log(inputRef)
   }, [])
   useEffect(() => {
+    console.log({ UPLOAD_PRESET, CLOUD_NAME, API_KEY })
     inputRef.current.on('sending', (file, xhr, formData) => {
-      formData.append('upload_preset', 'j67o9iln')
+      formData.append('upload_preset', UPLOAD_PRESET)
       formData.append('timestamp', Date.now() / 1000)
-      formData.append('api_key', 717733438377878)
+      formData.append('api_key', API_KEY)
     })
-  }, [inputRef])
-  useEffect(() => {
+    inputRef.current.on('error', (file, response) => {
+      console.log('HA IDO MAL')
+      console.log(response)
+    })
     inputRef.current.on('success', (file, response) => {
       const { public_id: publicId, secure_url: url } = response
 
       console.log({ publicId, url })
     })
   }, [inputRef])
-  useEffect(() => {
-    inputRef.current.on('error', (file, response) => {
-      console.log('HA IDO MAL')
-      console.log(response)
-    })
-  }, [inputRef])
 
   return (
-        <div className="m-12 lg:m-44">
-            <form
-                id="dropzone"
-                className="shadow-2xl border-dashed border-2 border-gray-300 rounded-lg aspect-video w-full flex items-center justify-center flex-col"
-                action="https://api.cloudinary.com/v1_1/midudev/image/upload"
-            >
-                <button className="font-bold pointer-events-none bg-blue-600 rounded-full text-bold text-white text-xl px-6 py-4">
-                    Upload files
-                </button>
-            </form>
-        </div>
+    <div className="m-12 lg:m-44">
+      <form
+        id="dropzone"
+        className="shadow-2xl border-dashed border-2 border-gray-300 rounded-lg aspect-video w-full flex items-center justify-center flex-col"
+        action={`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUDNAME}/image/upload`}
+      >
+        <button className="m-4 font-bold pointer-events-none bg-blue-600 rounded-full text-bold text-white text-xl px-6 py-4">
+          Upload files
+        </button>
+      </form>
+    </div>
   )
 }
 
